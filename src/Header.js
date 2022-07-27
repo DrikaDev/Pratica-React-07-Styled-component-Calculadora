2b7b9c593dbbb00bd25d592fb7aa3c4c2ef752c6
DrikaDev <adriguti.adriana@hotmail.com>
2022-07-27 18:13:01 -0300

    Pratica-React-07-Styled-component-Calculadora

---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react"
+import Foguete from './img/foguete.png';
+import styled from 'styled-components';
+
+// export const CaixaSuperior = styled.div`
+//     h1{
+//     color: white;
+//     font-size: 1.5rem;
+//     text-transform: uppercase;
+//     }
+
+//     img{
+//     width: 4rem;
+//     }
+// `
+
+// export default class Header extends Component{
+//     render(){
+//         return(
+//             <CaixaSuperior>
+//                 <h1>Calculadora</h1>
+//                 <img src={Foguete} alt= "Foguete Vai Na Web"/>
+//             </CaixaSuperior>
+//         )
+//     }
+// }
